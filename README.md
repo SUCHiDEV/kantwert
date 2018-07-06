b9abe13b8c02e1536c400b07e4720f5a06aea0c7
suchidev <dawid.tmk@gmail.com>
2018-07-06 09:36:28 +0200

    add: readme

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 1. Install node.js: https://nodejs.org/en/download/package-manager/
 
-2. 
+2. Init server: node server
+
+3. Open browser: 127.0.0.1:8888
 
 
 
--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-sudo apt install curl
-sudo curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -y nodejs
 sudo npm install
 sudo npm install -g
 sudo npm install forever -g
